26b4abafe19f45e5099e45c25636988cbc4dd42b
philip von mueffling <philip.vm3@gmail.com>
2020-11-02 16:41:30 -0500

    boolean add

--- SuperArray.java
+++ SuperArray.java
@@ -5,5 +5,18 @@ public class SuperArray{
     size =0;
     data=new String[10];
   }
+  public int size(){
+    return size;
+  }
+  public boolean add(String element){
+    if (size>=data.length){
+      return false;
+    }else {
+      data[size]=element;
+      size+=1;
+      return true;
+    }
+    
 
-}
+    }
+  }
